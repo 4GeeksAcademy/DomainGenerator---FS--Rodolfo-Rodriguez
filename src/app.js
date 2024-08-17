b9b7cbc36d8919ba829983b9e8db03f0c56c72ea
ThisIsMeFito <167917432+ThisIsMeFito@users.noreply.github.com>
2024-08-17 15:59:14 +0000

    [myDomainGenerator - FS Spain - Rodolfo Rodriguez]

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".es", ".net"];
+  // []
+  function DomainGen() {
+    let myDomain = [];
+    for (let p = 0; p < pronoun.length; p++) {
+      for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+          for (let e = 0; e < extension.length; e++) {
+            myDomain.push(pronoun[p] + adj[a] + noun[n] + extension[e]);
+          }
+        }
+      }
+    }
+    return myDomain;
+  }
+  console.log(DomainGen());
 };
